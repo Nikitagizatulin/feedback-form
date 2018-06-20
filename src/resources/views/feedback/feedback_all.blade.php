26faa3d625d24c3fe195656c5e3072d9bdc19bf7
Nikita <gizatulin_nb@groupbwt.com>
2018-06-20 23:38:34 +0300

    remove .idea

--- src/resources/views/feedback/feedback_all.blade.php
+++ src/resources/views/feedback/feedback_all.blade.php
@@ -6,9 +6,9 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <div class="box">
+            <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Bordered Table</h3>
+                    <h3 class="box-title">All of bids</h3>
                 </div>
                 <!-- /.box-header -->
                 <div class="box-body">
